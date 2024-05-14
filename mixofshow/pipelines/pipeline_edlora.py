ade6b70a89ada2b26b71408116f100eac50c9fec
Yuhang Xiao <mydcxiao@iGpu15.usc.edu>
2024-05-13 20:40:05 -0700

    remove unused kwargs

--- mixofshow/pipelines/pipeline_edlora.py
+++ mixofshow/pipelines/pipeline_edlora.py
@@ -38,9 +38,6 @@ class EDLoRAPipeline(StableDiffusionPipeline):
         tokenizer: CLIPTokenizer,
         unet: UNet2DConditionModel,
         scheduler: KarrasDiffusionSchedulers,
-        safety_checker=None,
-        feature_extractor=None,
-        requires_safety_checker: bool = False,
     ):
         if hasattr(scheduler.config, 'steps_offset') and scheduler.config.steps_offset != 1:
             deprecation_message = (
